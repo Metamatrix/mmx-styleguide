f41be4b9fa3e6ff8c92582ffb57b5f00947a21da
Sandra <sandra@magdalena>
2016-10-05 16:46:20 +0200

    Added option flags to JS validation and created dev-validation task

--- Gruntfile.js
+++ Gruntfile.js
@@ -201,8 +201,21 @@ module.exports = function(grunt) {
         reporterOutput: 'reports/js/jshint-report.html',
         force: false, //If set to true it will report errors but not fail the task
         ignores: ['src/scripts/vendor/*.js', 'src/scripts/styleguide/*.min.js'],
+        undef: true, //enable warnings about undefined variables
+        unused: true, //enable warnings about unused variables
+        bitwise: true, //prohibits the use of bitwise operators such as "|", as they are often mistyped.
+        curly: true, //requires you to always put curly braces around blocks in loops & conditionals
+        eqeqeq: true, //prohibits the use of == and != instead of === and !==
+        //esversion: 3, //specifies ES version to which the code must adhere. 3 = program needs to be executable in older browsers.
+        latedef: true, //prohibits the use of a variable before it was defined.
+        futurehostile: true, //enables warnings about the use of identifiers which are defined in future versions of JS.
+        freeze: true, //prohibits overwriting prototypes of native objects such as Array, Date and so on.
+        nonbsp: true, //warns about "non-breaking whitespace" characters that have a potential of breaking non-UTF8 web pages.
         globals: {
-          jQuery: true
+          jQuery: true,
+          $: true,
+          window: true,
+          document: true
         }
       },
       ignore_warning: {
@@ -230,10 +243,19 @@ module.exports = function(grunt) {
     'jade'
   ]);
 
+  grunt.registerTask('html-validation', [
+    'jade',
+    'validation'
+  ]);
+
   /**
    * JavaScript tasks
    */
   grunt.registerTask('javascript', [
+    'uglify:dev'
+  ]);
+
+  grunt.registerTask('javascript-validation', [
     'jshint',
     'uglify:dev'
   ]);
@@ -252,6 +274,14 @@ module.exports = function(grunt) {
     'css',
     'html',
     'javascript',
+    'images',
+    'copy',
+  ]);
+
+  grunt.registerTask('dev-validation', [
+    'css',
+    'html-validation',
+    'javascript-validation',
     'images',
     'copy',
   ]);
