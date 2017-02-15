111d519c9fc547787e06a8bc19335f6fad46234e
Andreas Nylin <andreas.nylin@gmail.com>
2017-02-15 06:55:38 +0100

    Added svg to images

--- grunt/watch.js
+++ grunt/watch.js
@@ -25,7 +25,7 @@ module.exports = function(grunt, options){
     //   }
     // },
     images: {
-      files: ['src/**/*.{png,jpg,gif}'],
+      files: ['src/**/*.{png,jpg,gif,svg}'],
       tasks: ['images']
     },
     copy: {
