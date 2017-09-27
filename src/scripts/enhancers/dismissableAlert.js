4e1cef4eb053186fae7040f628e55936e08223f3
Andreas Nylin <andreas.nylin@gmail.com>
2017-09-27 08:32:01 +0200

    Moved bootstrap extensions to styleguide

---
+++ src/scripts/enhancers/dismissableAlert.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+
+    "use strict";
+
+    $.enhancer("dismissableAlert", function () {
+
+    	var alert = $(this);
+
+      alert.on("closed.bs.alert", function () {
+      	if(alert.data("dismissable-id")) {
+      		var cookieId = alert.data("dismissable-id") + "-closed";
+        	$.cookies.set(cookieId, "true");
+      	}
+      });
+      
+    });
+
+})(jQuery);
