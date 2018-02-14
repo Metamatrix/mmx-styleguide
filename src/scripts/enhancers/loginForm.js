2e473bb7d2a83d82513c62f17c97129aea12da24
Mats Lindblad <mats.lindblad@gmail.com>
2018-02-14 15:31:09 +0100

    Added js enhancers for BankId

---
+++ src/scripts/enhancers/loginForm.js
@@ -0,0 +1,28 @@
+﻿(function(app, $) {
+	$.enhancer("loginForm", function() {
+
+		var loginType = $(this).find("select"),
+			ssn = $("#LoginViewModel_PersonalNumber"),
+			ssnWrapper = ssn.closest(".collapse");
+
+		// Hide and show social security nr field depending on the type of bankid selected 
+		loginType.on("change", function() {
+
+			if ($(this).val() === "1") {
+				ssnWrapper.collapse("hide");
+			} else {
+				ssnWrapper.collapse("show");
+			}
+
+		});
+
+		// Set mobile bank id as default if using a small screen
+		if (Modernizr.mq("(max-width:767px)")) {
+			loginType.val("0");
+			ssnWrapper.collapse("show");
+		}
+		else if (Modernizr.mq("(min-width:768px)")) {
+			ssnWrapper.removeClass("in");
+		}
+	});
+})(Application, jQuery);
---
+++ src/scripts/enhancers/loginStatus.js
@@ -0,0 +1,92 @@
+﻿(function (app, $, win) {
+    "use strict";
+
+    var serviceUrl = "",
+        bankIdUrl = "",
+        pollDelay = 1000,
+        autostart = false,
+        polling = false,
+        loginType = "1",
+        interval,
+        startButtonDelay = 5;
+
+    function updateStatus() {
+
+        if (!polling) {
+
+            polling = true;
+
+            $.ajax({
+                cache: false,
+                data: "loginType=" + loginType,
+                url: serviceUrl,
+                dataType: "json",
+                success: statusReceived,
+                error: function () {
+                    polling = false;
+                }
+            });
+        };
+    }
+
+    function tryStartBankIdApp() {
+        location.href = bankIdUrl;
+    }
+
+    function statusReceived(response) {
+
+        if (response.ReturnUrl) {
+            win.clearInterval(interval);
+            $("#autostart-wrapper").addClass("hidden");
+        }
+
+        if (response.ReturnUrl) {
+            win.setTimeout(function () {
+                    location.href = response.ReturnUrl;
+                }, pollDelay);
+        }
+
+        if (response.Message) {
+            $("#login-status").text(response.Message);
+        }
+
+        if (response.Error) {
+            $("#login-status-container").hide();
+            $("#login-error-container").show();
+            $("#login-error").text(response.Message);
+        }
+        if (response.CancelPoll) {
+            clearInterval(interval);
+        }
+        polling = false;
+    }
+
+    $.enhancer("loginStatus", function () {
+
+        var element = $(this);
+
+        serviceUrl = element.data("service-url");
+        bankIdUrl = element.data("bankid-url");
+        autostart = element.data("autostart");
+        loginType = element.data("logintype");
+
+        if (element.data("startbutton-delay")) {
+            startButtonDelay = Number(element.data("startbutton-delay"));
+        }
+
+        interval = win.setInterval(updateStatus, pollDelay);
+
+        if (autostart) {
+            setTimeout(tryStartBankIdApp, 1000);
+            setTimeout(function () {
+                $("#autostart-wrapper").removeClass("hidden");
+            }, startButtonDelay * 1000);
+        }
+
+        $("#btn-start-bankid").on("click", function () {
+            $(this).attr("disabled", "disabled");
+        });
+
+    });
+
+})(Application, jQuery, window);
